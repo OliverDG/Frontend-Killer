dc87842e5c9a8da60dbc08dd78c96c6576bf256b
OliverDG <oliverdgarciap@gmail.com>
2017-08-30 21:49:45 -0400

    gulpfile and src folder created

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+//Compile sass
+gulp.task('sass',function(){
+  return gulp.src(['src/sass/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+})
+
+//watch and server
+gulp.task('serve',['sass'], function(){
+  browserSync.init({
+    server:'./src'
+  });
+  gulp.watch(['src/sass/*.scss'],['sass']);
+  gulp.watch(['src/*.html']).on('change', browserSync.reload);
+});
+
+//iniciador de tareas
+gulp.task('default',['serve']);
